c5998e38d91fdacf273978da8dcbdcfb612cccce
AndyL <missedmemo@gmail.com>
2018-10-05 20:22:51 -0700

    iterate over arrays and object members

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ const _ = {};
 module.exports = _;
 
 _.each = (collection, callback) => {
-  for (let i = 0; i < collection.length; i++) {
-    callback(collection[i], i, collection);
+  if (Array.isArray(collection)) {
+    for (let i = 0; i < collection.length; i++) {
+      callback(collection[i], i, collection);
+    }
+  } else {
+    for (let key in collection) {
+      callback(collection[key], key, collection);
+    }
   }
+
+  return collection; // mostly useless, but standard, for chaining
 };
--- test/index.test.js
+++ test/index.test.js
@@ -2,8 +2,36 @@ const _ = require("../src");
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+const obj = {
+  name: "Bob",
+  age: 24,
+  phone: null,
+  email: "bob@example.com"
+};
+
 describe("lowbar helper methods", () => {
   //
+  describe("each", () => {
+    //
+    it("should iterate over all array members", () => {
+      const results = [];
+      _.each(numbers, n => results.push(n));
+      expect(results).toEqual(numbers);
+    });
+
+    it("should iterate over all object members", () => {
+      let str = "";
+
+      _.each(obj, item => {
+        str += item ? item.toString() : "";
+      });
+
+      const expected = Object.values(obj).join("");
+      expect(str).toEqual(expected);
+    });
+  });
+
+  /*
   describe("each", () => {
     //
     it("should iterate over all members", () => {
@@ -12,4 +40,5 @@ describe("lowbar helper methods", () => {
       expect(results).toEqual(numbers);
     });
   });
+  */
 });
