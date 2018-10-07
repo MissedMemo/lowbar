b1eb1fb31e92a67392c2ab196ae2285ccbffc605
AndyL <missedmemo@gmail.com>
2018-10-07 16:25:20 -0700

    babelize the build

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-const _ = {};
-
-module.exports = _;
-
-_.each = (collection, callback) => {
-  if (Array.isArray(collection)) {
-    for (let i = 0; i < collection.length; i++) {
-      callback(collection[i], i, collection);
+export default {
+  //
+  each: (collection, callback) => {
+    if (Array.isArray(collection)) {
+      for (let i = 0; i < collection.length; i++) {
+        callback(collection[i], i, collection);
+      }
+    } else {
+      for (let key in collection) {
+        callback(collection[key], key, collection);
+      }
     }
-  } else {
-    for (let key in collection) {
-      callback(collection[key], key, collection);
-    }
+
+    return collection; // mostly useless, but standard, for chaining
   }
-
-  return collection; // mostly useless, but standard, for chaining
 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
-const _ = require("../src");
+const _ = require("../lib").default;
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
