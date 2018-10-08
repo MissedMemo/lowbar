6f275770216b086fc4004271b28c775d633fe80b
AndyL <missedmemo@gmail.com>
2018-10-08 09:31:17 -0700

    implement map, filter, & reduce

--- src/index.js
+++ src/index.js
@@ -1,16 +1,50 @@
-export default {
-  //
-  each: (collection, callback) => {
-    if (Array.isArray(collection)) {
-      for (let i = 0; i < collection.length; i++) {
-        callback(collection[i], i, collection);
-      }
-    } else {
-      for (let key in collection) {
-        callback(collection[key], key, collection);
-      }
-    }
+const _ = {};
 
-    return collection; // mostly useless, but standard, for chaining
+export default _;
+
+_.each = (collection, callback) => {
+  //
+  if (Array.isArray(collection)) {
+    for (let i = 0; i < collection.length; i++) {
+      callback(collection[i], i, collection);
+    }
+  } else {
+    for (let key in collection) {
+      callback(collection[key], key, collection);
+    }
   }
+
+  return collection; // mostly useless, but standard, for chaining
+};
+
+_.map = (collection, callback) => {
+  const results = [];
+
+  _.each(collection, (element, i) => {
+    results.push(callback(element, i, collection));
+  });
+  return results;
+};
+
+_.filter = (collection, callback) => {
+  const results = [];
+
+  _.each(collection, (element, i) => {
+    if (callback(element, i, collection)) {
+      results.push(element);
+    }
+  });
+
+  return results;
+};
+
+_.reduce = (collection, callback, memo) => {
+  _.each(collection, (value, i) => {
+    if (memo === undefined) {
+      memo = value;
+    } else {
+      memo = callback(memo, value, i, collection);
+    }
+  });
+  return memo;
 };
--- test/index.test.js
+++ test/index.test.js
@@ -13,13 +13,13 @@ describe("lowbar helper methods", () => {
   //
   describe("each", () => {
     //
-    it("should iterate over all array members", () => {
+    it("should iterate over all ARRAY members", () => {
       const results = [];
       _.each(numbers, n => results.push(n));
       expect(results).toEqual(numbers);
     });
 
-    it("should iterate over all object members", () => {
+    it("should iterate over all OBJECT members", () => {
       let str = "";
 
       _.each(obj, item => {
@@ -31,14 +31,50 @@ describe("lowbar helper methods", () => {
     });
   });
 
-  /*
-  describe("each", () => {
+  describe("map", () => {
     //
-    it("should iterate over all members", () => {
-      const results = [];
-      _.each(numbers, n => results.push(n));
-      expect(results).toEqual(numbers);
+    it("should apply transform to ARRAY elements", () => {
+      const transform = n => n * 2;
+      const results = _.map(numbers, transform);
+      expect(results).toEqual(numbers.map(transform));
+    });
+
+    it("should apply transform to OBJECT elements", () => {
+      const transform = o => `${o || ""}`;
+      const results = _.map(obj, transform).join("");
+      const expected = Object.values(obj).join("");
+
+      expect(results).toEqual(expected);
     });
   });
-  */
+
+  describe("filter", () => {
+    //
+    it("should return specified subset of ARRAY elements", () => {
+      const isEven = n => n % 2 == 0;
+      const results = _.filter(numbers, isEven);
+      expect(results).toEqual(numbers.filter(isEven));
+    });
+
+    it("should return specified subset of OBJECT values", () => {
+      const isNumber = item => typeof item === "number";
+      const results = _.filter(obj, isNumber);
+      expect(results).toEqual([obj.age]);
+    });
+  });
+
+  describe("reduce", () => {
+    //
+    const reducer = (total, n) => n + total;
+
+    it("should reduce ARRAY elements to a single result", () => {
+      const sum = _.reduce(numbers, reducer);
+      expect(sum).toEqual(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
+    });
+
+    it("should incorporate initial starting value", () => {
+      const sum = _.reduce(numbers, reducer, 5);
+      expect(sum).toEqual(5 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
+    });
+  });
 });
